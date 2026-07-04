e7f611a83e0b872941b44f1f605c14533a07395b
Nguyễn Kiên <nguyenkieniski@gmail.com>
2026-07-05 04:00:45 +0700

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -20,9 +20,10 @@ class WelcomeCog(commands.Cog):
                 embed = discord.Embed(
                     title="Aloha Mem Mới Nha! ✨",
                     description=(
-                        f"Rất vui vì {member.mention} đã tìm đến Angelic ໒꒱. Hãy đọc luật tại <#1512135955983630617>\n"
-                        f"chọn roles mà bạn muốn tại <#1498731263304007710> , thả lỏng, làm quen với mọi người tại <#1498711783223853101> "
-                        f"và biến nơi đây thành ngôi nhà thứ hai của mình nhé."
+                        f"Rất vui vì {member.mention} đã tìm đến Angelic ໒꒱.\n\n"
+                        f"Hãy đọc luật tại <#1512135955983630617>.\n\n"
+                        f"Chọn roles mà bạn muốn tại <#1498731263304007710>.\n\n"
+                        f"Thả lỏng, làm quen với mọi người tại <#1498711783223853101> và biến nơi đây thành ngôi nhà thứ hai của mình nhé."
                     ),
                     color=0xffb6c1
                 )
